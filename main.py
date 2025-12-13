86d59db4fe52105264ced6bd672308806e426e13
arminasle-cmyk <arminasle@gmail.com>
2025-12-13 17:02:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,4 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
 
-app = FastAPI()
-
-
-@app.get("/health")
-def health():
-    return {
-        "status": "OK",
-        "service": "Astrology API",
-        "version": "1.0"
-    }
-
-
-class BirthData(BaseModel):
-    date: str      # YYYY-MM-DD
-    time: str      # HH:MM
-    timezone: str  # pvz. Europe/Vilnius
-    lat: float
-    lon: float
-
-
-@app.post("/calculate")
-def calculate(data: BirthData):
-    return {
-        "status": "OK",
-        "received": data
-    }
 
 
 

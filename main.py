8a732a2e7ea0981b092e25280c7288222711bd4a
arminasle-cmyk <arminasle@gmail.com>
2025-12-13 16:37:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"status": "ROOT_OK"}
 
 @app.get("/health")
 def health():
-    return {"status": "OK_FORCE"}
+    return {
+        "status": "OK",
+        "service": "Astrology API",
+        "version": "1.0"
+    }
+
+
+class BirthData(BaseModel):
+    date: str      # YYYY-MM-DD
+    time: str      # HH:MM
+    timezone: str  # pvz. Europe/Vilnius
+    lat: float
+    lon: float
+
+
+@app.post("/calculate")
+def calculate(data: BirthData):
+    return {
+        "status": "OK",
+        "received": data
+    }
+
